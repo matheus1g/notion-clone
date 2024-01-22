0348030e70cbc37f82f27b3bb5006fb4cf41ab85
Matheus Gomes <matheusgomestosta8@gmail.com>
2024-01-22 16:08:14 -0300

    document list

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,15 +6,15 @@ import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import UserItem from "./user-item";
-import { useMutation, useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import Item from "./item";
 import { toast } from "sonner";
+import DocumentList from "./document-list";
 
 const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px")
-  const documemts = useQuery(api.documents.get);
   const create = useMutation(api.documents.create)
 
   const isResizingRef = useRef(false);
@@ -147,11 +147,7 @@ const Navigation = () => {
           />
         </div>
         <div className="mt-4">
-          {documemts?.map((document)=> (
-            <p key={document._id}>
-              {document.title}
-            </p>
-          ))}
+          <DocumentList />
         </div>
         <div
           onMouseDown={handleMouseDown}
