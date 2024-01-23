bc57bc5fb2dc3da272b0a1363b81b1a513375719
Matheus Gomes <matheusgomestosta8@gmail.com>
2024-01-23 13:17:29 -0300

    Archive Functionality

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from "lucide-react";
+import { ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
@@ -148,6 +148,11 @@ const Navigation = () => {
         </div>
         <div className="mt-4">
           <DocumentList />
+          <Item 
+            onClick={handleCreate} 
+            icon={Plus}
+            label="Add a page"
+          />
         </div>
         <div
           onMouseDown={handleMouseDown}
